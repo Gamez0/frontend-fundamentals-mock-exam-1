da4fb0af2c680e705c163be10e2d5d43320798d6
dobin <dobinshin@gmail.com>
2026-04-06 23:07:43 +0900

    feat: SavingsFormProvider 페이지에 적용

--- src/pages/savings-calculator/SavingsCalculatorPage.tsx
+++ src/pages/savings-calculator/SavingsCalculatorPage.tsx
@@ -1,5 +1,6 @@
 import { Assets, Border, colors, ListRow, NavigationBar, Spacing, Tab } from 'tosslib';
 import SavingsForm from './SavingsForm/SavingsForm';
+import SavingsFormProvider from './provider/SavingsFormProvider';
 
 export function SavingsCalculatorPage() {
   return (
@@ -7,54 +8,54 @@ export function SavingsCalculatorPage() {
       <NavigationBar title="적금 계산기" />
 
       <Spacing size={16} />
+      <SavingsFormProvider>
+        <SavingsForm />
 
-      <SavingsForm />
+        <Spacing size={24} />
+        <Border height={16} />
+        <Spacing size={8} />
 
-      <Spacing size={24} />
-      <Border height={16} />
-      <Spacing size={8} />
+        <Tab onChange={() => {}}>
+          <Tab.Item value="products" selected={true}>
+            적금 상품
+          </Tab.Item>
+          <Tab.Item value="results" selected={false}>
+            계산 결과
+          </Tab.Item>
+        </Tab>
 
-      <Tab onChange={() => {}}>
-        <Tab.Item value="products" selected={true}>
-          적금 상품
-        </Tab.Item>
-        <Tab.Item value="results" selected={false}>
-          계산 결과
-        </Tab.Item>
-      </Tab>
+        <ListRow
+          contents={
+            <ListRow.Texts
+              type="3RowTypeA"
+              top={'기본 정기적금'}
+              topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
+              middle={'연 이자율: 3.2%'}
+              middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
+              bottom={'100,000원 ~ 500,000원 | 12개월'}
+              bottomProps={{ fontSize: 13, color: colors.grey600 }}
+            />
+          }
+          right={<Assets.Icon name="icon-check-circle-green" />}
+          onClick={() => {}}
+        />
+        <ListRow
+          contents={
+            <ListRow.Texts
+              type="3RowTypeA"
+              top={'고급 정기적금'}
+              topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
+              middle={'연 이자율: 2.8%'}
+              middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
+              bottom={'50,000원 ~ 1,000,000원 | 24개월'}
+              bottomProps={{ fontSize: 13, color: colors.grey600 }}
+            />
+          }
+          onClick={() => {}}
+        />
 
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="3RowTypeA"
-            top={'기본 정기적금'}
-            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-            middle={'연 이자율: 3.2%'}
-            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-            bottom={'100,000원 ~ 500,000원 | 12개월'}
-            bottomProps={{ fontSize: 13, color: colors.grey600 }}
-          />
-        }
-        right={<Assets.Icon name="icon-check-circle-green" />}
-        onClick={() => {}}
-      />
-      <ListRow
-        contents={
-          <ListRow.Texts
-            type="3RowTypeA"
-            top={'고급 정기적금'}
-            topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-            middle={'연 이자율: 2.8%'}
-            middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-            bottom={'50,000원 ~ 1,000,000원 | 24개월'}
-            bottomProps={{ fontSize: 13, color: colors.grey600 }}
-          />
-        }
-        onClick={() => {}}
-      />
-
-      {/* 아래는 계산 결과 탭 내용이에요. 계산 결과 탭을 구현할 때 주석을 해제해주세요. */}
-      {/* <Spacing size={8} />
+        {/* 아래는 계산 결과 탭 내용이에요. 계산 결과 탭을 구현할 때 주석을 해제해주세요. */}
+        {/* <Spacing size={8} />
 
       <ListRow
         contents={
@@ -128,8 +129,9 @@ export function SavingsCalculatorPage() {
 
       <Spacing size={40} /> */}
 
-      {/* 아래는 사용자가 적금 상품을 선택하지 않고 계산 결과 탭을 선택했을 때 출력해주세요. */}
-      {/* <ListRow contents={<ListRow.Texts type="1RowTypeA" top="상품을 선택해주세요." />} /> */}
+        {/* 아래는 사용자가 적금 상품을 선택하지 않고 계산 결과 탭을 선택했을 때 출력해주세요. */}
+        {/* <ListRow contents={<ListRow.Texts type="1RowTypeA" top="상품을 선택해주세요." />} /> */}
+      </SavingsFormProvider>
     </>
   );
 }
