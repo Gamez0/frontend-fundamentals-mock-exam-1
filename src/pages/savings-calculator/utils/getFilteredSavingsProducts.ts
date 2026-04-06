7134685be0937c8ade1e8f4b2179182bf39fe21b
dobin <dobinshin@gmail.com>
2026-04-07 02:19:24 +0900

    refactor: filter 함수 list를 생성하는 헬퍼 함수 구현해 확장성 확보

--- src/pages/savings-calculator/utils/getFilteredSavingsProducts.ts
+++ src/pages/savings-calculator/utils/getFilteredSavingsProducts.ts
@@ -1,51 +1,25 @@
 import { isInRange } from '@/shared/utils/isInRange';
 import type { SavingsProduct, SavingsProductFilterOption } from '../savings-calculator.types';
 
+type ProductFilter = (product: SavingsProduct) => boolean;
+
 interface GetFilteredSavingsProductsParams {
   savingsProducts: SavingsProduct[];
   filterOption: SavingsProductFilterOption;
 }
 
 export const getFilteredSavingsProducts = ({ savingsProducts, filterOption }: GetFilteredSavingsProductsParams) => {
-  const { monthlyAmount, savingsPeriod } = filterOption;
-
-  const monthlyFilteredSavings = filterProductsByMonthlyAmount({
-    savingsProducts,
-    monthlyAmount,
-  });
-
-  const periodFilteredSavings = filterProductsBySavingsPeriod({
-    savingsProducts: monthlyFilteredSavings,
-    savingsPeriod,
-  });
-
-  return periodFilteredSavings;
+  const filters = createFilters(filterOption);
+  return savingsProducts.filter(product => filters.every(f => f(product)));
 };
 
-interface FilterProductsByMonthlyAmountParams {
-  savingsProducts: SavingsProduct[];
-  monthlyAmount: number;
-}
+const createFilters = (filterOption: SavingsProductFilterOption): ProductFilter[] => {
+  const { monthlyAmount, savingsPeriod } = filterOption;
 
-function filterProductsByMonthlyAmount({ savingsProducts, monthlyAmount }: FilterProductsByMonthlyAmountParams) {
-  if (monthlyAmount === 0) {
-    return savingsProducts;
-  }
-
-  return savingsProducts.filter(product =>
-    isInRange(monthlyAmount, product.minMonthlyAmount, product.maxMonthlyAmount)
-  );
-}
-
-interface FilterProductsBySavingsPeriodParams {
-  savingsProducts: SavingsProduct[];
-  savingsPeriod: number;
-}
-
-function filterProductsBySavingsPeriod({ savingsProducts, savingsPeriod }: FilterProductsBySavingsPeriodParams) {
-  if (savingsPeriod === 0) {
-    return savingsProducts;
-  }
-
-  return savingsProducts.filter(product => product.availableTerms === savingsPeriod);
-}
+  return [
+    monthlyAmount === 0
+      ? null
+      : (p: SavingsProduct) => isInRange(monthlyAmount, p.minMonthlyAmount, p.maxMonthlyAmount),
+    savingsPeriod === 0 ? null : (p: SavingsProduct) => p.availableTerms === savingsPeriod,
+  ].filter(Boolean) as ProductFilter[];
+};
