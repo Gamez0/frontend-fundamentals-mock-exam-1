37f82d95422be65a6ad320ba06e2761c7ab5cf9c
dobin <dobinshin@gmail.com>
2026-04-06 16:35:38 +0900

    refactor: 목표금액, 월 납입액, 저축 기간 입력 영역 SavingsForm 컴포넌트로 분리

--- src/pages/savings-calculator/SavingsCalculatorPage.tsx
+++ src/pages/savings-calculator/SavingsCalculatorPage.tsx
@@ -1,15 +1,5 @@
-import {
-  Assets,
-  Border,
-  colors,
-  ListHeader,
-  ListRow,
-  NavigationBar,
-  SelectBottomSheet,
-  Spacing,
-  Tab,
-  TextField,
-} from 'tosslib';
+import { Assets, Border, colors, ListRow, NavigationBar, Spacing, Tab } from 'tosslib';
+import SavingsForm from './SavingsForm/SavingsForm';
 
 export function SavingsCalculatorPage() {
   return (
@@ -18,15 +8,7 @@ export function SavingsCalculatorPage() {
 
       <Spacing size={16} />
 
-      <TextField label="목표 금액" placeholder="목표 금액을 입력하세요" suffix="원" />
-      <Spacing size={16} />
-      <TextField label="월 납입액" placeholder="희망 월 납입액을 입력하세요" suffix="원" />
-      <Spacing size={16} />
-      <SelectBottomSheet label="저축 기간" title="저축 기간을 선택해주세요" value={12} onChange={() => {}}>
-        <SelectBottomSheet.Option value={6}>6개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={12}>12개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={24}>24개월</SelectBottomSheet.Option>
-      </SelectBottomSheet>
+      <SavingsForm />
 
       <Spacing size={24} />
       <Border height={16} />
