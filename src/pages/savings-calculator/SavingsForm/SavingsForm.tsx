67067555e3bf3b0c32823706c78a5b2cf9b14796
dobin <dobinshin@gmail.com>
2026-04-06 23:44:32 +0900

    feat: 입력 영역에 TextField/SelectFieldController 적용

--- src/pages/savings-calculator/SavingsForm/SavingsForm.tsx
+++ src/pages/savings-calculator/SavingsForm/SavingsForm.tsx
@@ -1,17 +1,29 @@
-import { SelectBottomSheet, Spacing, TextField } from 'tosslib';
+import SelectFieldController from '@/shared/components/SelectFieldController';
+import TextFieldController from '@/shared/components/TextFieldController';
+import { Spacing } from 'tosslib';
 
 export default function SavingsForm() {
   return (
-    <form>
-      <TextField label="목표 금액" placeholder="목표 금액을 입력하세요" suffix="원" />
+    <form onSubmit={e => e.preventDefault()}>
+      <TextFieldController name="savingsType" label="목표 금액" placeholder="적금 종류를 입력하세요" suffix="원" />
       <Spacing size={16} />
-      <TextField label="월 납입액" placeholder="희망 월 납입액을 입력하세요" suffix="원" />
+      <TextFieldController
+        name="interestRate"
+        label="월 납입액"
+        placeholder="희망 월 납입액을 입력하세요"
+        suffix="원"
+      />
       <Spacing size={16} />
-      <SelectBottomSheet label="저축 기간" title="저축 기간을 선택해주세요" value={12} onChange={() => {}}>
-        <SelectBottomSheet.Option value={6}>6개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={12}>12개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={24}>24개월</SelectBottomSheet.Option>
-      </SelectBottomSheet>
+      <SelectFieldController
+        name="savingsPeriod"
+        label="저축 기간"
+        title="저축 기간을 선택하세요"
+        options={[
+          { label: '6개월', value: 6 },
+          { label: '12개월', value: 12 },
+          { label: '24개월', value: 24 },
+        ]}
+      />
     </form>
   );
 }
