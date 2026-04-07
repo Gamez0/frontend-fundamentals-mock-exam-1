18c9f090153530abcefb3290bd1c6a9a86bd6097
dobin <dobinshin@gmail.com>
2026-04-07 16:55:25 +0900

    feat: 적금 상품 리스트 탭 구현

---
+++ src/pages/savings-calculator/containers/SavingsTabSection/SavingsProductsTab/SavingsProductItem.tsx
@@ -0,0 +1,38 @@
+import { SavingsProduct } from '@/pages/savings-calculator/savings-calculator.types';
+import { useSelectedProductId, useSetSelectedProduct } from '@/pages/savings-calculator/stores/useSelectedProduct';
+import { Assets, colors, ListRow } from 'tosslib';
+interface SavingsProductItemProps {
+  product: SavingsProduct;
+}
+export default function SavingsProductItem({ product }: SavingsProductItemProps) {
+  const selectedProductId = useSelectedProductId();
+  const setSelectedProduct = useSetSelectedProduct();
+
+  const handleProductClick = (productId: string) => {
+    if (selectedProductId === productId) {
+      setSelectedProduct(null);
+      return;
+    }
+    setSelectedProduct(productId);
+  };
+
+  return (
+    <ListRow
+      contents={
+        <ListRow.Texts
+          type="3RowTypeA"
+          top={product.name}
+          topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
+          middle={`연 이자율: ${product.annualRate}%`}
+          middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
+          bottom={`${product.minMonthlyAmount.toLocaleString()}원 ~ ${product.maxMonthlyAmount.toLocaleString()}원 | ${
+            product.availableTerms
+          }개월`}
+          bottomProps={{ fontSize: 13, color: colors.grey600 }}
+        />
+      }
+      right={selectedProductId === product.id && <Assets.Icon name="icon-check-circle-green" />}
+      onClick={() => handleProductClick(product.id)}
+    />
+  );
+}
---
+++ src/pages/savings-calculator/containers/SavingsTabSection/SavingsProductsTab/SavingsProductsTab.tsx
@@ -0,0 +1,38 @@
+import { useFilteredSavingsProducts } from '@/pages/savings-calculator/quries/savings-products-queries';
+import { Suspense } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { ListRow } from 'tosslib';
+import SavingsProductItem from './SavingsProductItem';
+
+export default function SavingsProductsTab() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SavingsProductsList />
+    </Suspense>
+  );
+}
+
+function SavingsProductsList() {
+  const { watch } = useFormContext();
+  const monthlyAmount = watch('monthlyAmount');
+  const savingsPeriod = watch('savingsPeriod');
+  const filterOption = {
+    monthlyAmount,
+    savingsPeriod,
+  };
+
+  const { data: products } = useFilteredSavingsProducts(filterOption);
+
+  if (products.length === 0) {
+    // 입력도 하기 전에 이렇게 보여주는건 좀 아님
+    return <ListRow contents={<ListRow.Texts type="1RowTypeA" top="조건에 맞는 추천 상품이 없어요." />} />;
+  }
+
+  return (
+    <>
+      {products.map(product => (
+        <SavingsProductItem key={product.id} product={product} />
+      ))}
+    </>
+  );
+}
