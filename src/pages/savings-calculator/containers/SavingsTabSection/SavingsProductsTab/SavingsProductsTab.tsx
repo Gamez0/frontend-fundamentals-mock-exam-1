ea5da9760b7ffc96386b08cd999817d6812aa459
dobin <dobinshin@gmail.com>
2026-04-07 22:41:56 +0900

    chore: 주석 삭제

--- src/pages/savings-calculator/containers/SavingsTabSection/SavingsProductsTab/SavingsProductsTab.tsx
+++ src/pages/savings-calculator/containers/SavingsTabSection/SavingsProductsTab/SavingsProductsTab.tsx
@@ -26,7 +26,6 @@ function SavingsProductsList() {
   const { data: products } = useFilteredSavingsProducts(filterOption);
 
   if (products.length === 0) {
-    // 입력도 하기 전에 이렇게 보여주는건 좀 아님
     return <ListRow contents={<ListRow.Texts type="1RowTypeA" top="조건에 맞는 추천 상품이 없어요." />} />;
   }
 
