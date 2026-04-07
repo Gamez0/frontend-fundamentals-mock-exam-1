1f3f50fe81112860a5a9547fa223b6128bc330c1
dobin <dobinshin@gmail.com>
2026-04-07 16:54:37 +0900

    feat: useSelectedProduct store 선언

---
+++ src/pages/savings-calculator/stores/useSelectedProduct.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand/react';
+
+interface SelectedProductState {
+  selectedProductId: string | null;
+  setSelectedProduct: (id: string | null) => void;
+}
+
+const useSelectedProduct = create<SelectedProductState>(set => ({
+  selectedProductId: null,
+  setSelectedProduct: id => set({ selectedProductId: id }),
+}));
+
+export const useSelectedProductId = () => useSelectedProduct(state => state.selectedProductId);
+
+// export only setter
+export const useSetSelectedProduct = () => useSelectedProduct(state => state.setSelectedProduct);
