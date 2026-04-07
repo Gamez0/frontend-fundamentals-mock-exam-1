7c80ca009295129ee7b6c5cdaa23825423cb92ec
dobin <dobinshin@gmail.com>
2026-04-07 18:04:09 +0900

    fix: total -> target 으로 이름 수정

--- src/pages/savings-calculator/containers/SavingsForm/SavingsForm.tsx
+++ src/pages/savings-calculator/containers/SavingsForm/SavingsForm.tsx
@@ -1,12 +1,12 @@
 import { Spacing } from 'tosslib';
-import TotalAmountField from './TotalAmountField/TotalAmountField';
+import TargetAmountField from './TargetAmountField/TargetAmountField';
 import SavingsPeriodField from './SavingsPeriodField/SavingsPeriodField';
 import MonthlyAmountField from './MonthlyAmountField/MonthlyAmountField';
 
 export default function SavingsForm() {
   return (
     <form onSubmit={e => e.preventDefault()}>
-      <TotalAmountField />
+      <TargetAmountField />
       <Spacing size={16} />
       <MonthlyAmountField />
       <Spacing size={16} />
--- src/pages/savings-calculator/containers/SavingsForm/TargetAmountField/TargetAmountField.tsx
+++ src/pages/savings-calculator/containers/SavingsForm/TargetAmountField/TargetAmountField.tsx
@@ -2,10 +2,10 @@ import TextFieldController from '@/shared/components/TextFieldController';
 import { formatLocalePrice } from '@/shared/utils/formatLocalePrice';
 import { parseNumberFromString } from '@/shared/utils/parseNumberFromString';
 
-export default function TotalAmountField() {
+export default function TargetAmountField() {
   return (
     <TextFieldController
-      name="totalAmount"
+      name="targetAmount"
       label="목표 금액"
       placeholder="적금 종류를 입력하세요"
       suffix="원"
